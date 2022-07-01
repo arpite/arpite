311ed8e6ac4173d24891841e794f148aacf453cc
Domantas Mauruča <domantas.mauruca@gmail.com>
2022-07-01 11:48:00 +0200

    chore: remove PostCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,34 @@
-/**
- * More information: https://tailwindcss.com/docs/customizing-colors#using-css-variables
- */
-const withOpacityValue = (variable) => {
-	return ({ opacityValue }) => {
-		if (opacityValue === undefined) {
-			return `rgb(${variable})`;
-		}
+const defaultTheme = require('tailwindcss/defaultTheme');
 
-		return `rgb(${variable} / ${opacityValue})`;
-	};
-};
-
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
-	content: ["./resources/js/**/*.tsx"],
-	// content: ["./resources/views/app.blade.php", "./resources/js/**/*.tsx"],
+	content: ["./resources/js/**/*.{tsx,ts}"],
 	theme: {
 		screens: {
 			xs: "425px",
-			sm: "640px",
-			md: "768px",
-			lg: "1024px",
-			xl: "1280px",
-			"2xl": "1536px",
+			...defaultTheme.screens
 		},
 		extend: {
 			colors: {
 				primary: {
-					50: withOpacityValue`var(--arpite-primary-50, 236 253 245)`,
-					100: withOpacityValue`var(--arpite-primary-100, 209 250 229)`,
-					200: withOpacityValue`var(--arpite-primary-200, 167 243 208)`,
-					300: withOpacityValue`var(--arpite-primary-300, 110 231 183)`,
-					400: withOpacityValue`var(--arpite-primary-400, 52 211 153)`,
-					500: withOpacityValue`var(--arpite-primary-500, 16 185 129)`,
-					600: withOpacityValue`var(--arpite-primary-600, 5 150 105)`,
-					700: withOpacityValue`var(--arpite-primary-700, 4 120 87)`,
-					800: withOpacityValue`var(--arpite-primary-800, 6 95 70)`,
-					900: withOpacityValue`var(--arpite-primary-900, 6 78 59)`,
+					50: 'rgb(var(--arpite-primary-50, 236 253 245) / <alpha-value>)',
+					100: 'rgb(var(--arpite-primary-100, 209 250 229) / <alpha-value>)',
+					200: 'rgb(var(--arpite-primary-200, 167 243 208) / <alpha-value>)',
+					300: 'rgb(var(--arpite-primary-300, 110 231 183) / <alpha-value>)',
+					400: 'rgb(var(--arpite-primary-400, 52 211 153) / <alpha-value>)',
+					500: 'rgb(var(--arpite-primary-500, 16 185 129) / <alpha-value>)',
+					600: 'rgb(var(--arpite-primary-600, 5 150 105) / <alpha-value>)',
+					700: 'rgb(var(--arpite-primary-700, 4 120 87) / <alpha-value>)',
+					800: 'rgb(var(--arpite-primary-800, 6 95 70) / <alpha-value>)',
+					900: 'rgb(var(--arpite-primary-900, 6 78 59) / <alpha-value>)',
 				},
 			},
 			backgroundColor: {
 				background: "#F4F5F7",
 			},
 			fontFamily: {
-				base: '"Inter", sans-serif',
-			},
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+			}
 		},
 	},
 	plugins: [require("@tailwindcss/forms")],
